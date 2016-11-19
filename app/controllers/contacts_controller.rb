833137634cf4573bce25b6dc2dfbb60ba5c309f4
kortirso <kortirso@gmail.com>
2016-11-19 15:48:20 +0300

    add contacts index page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,6 @@ class ContactsController < ApplicationController
     before_action :get_access
     
     def index
-
+        @contacts = current_user.contacts
     end
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -8,10 +8,21 @@ RSpec.describe ContactsController, type: :controller do
 
         context 'When user logged in' do
             sign_in_user
+            let!(:contacts) { create_list(:contact, 2, user: @current_user) }
+            let!(:another_contact) { create :contact }
+            before { get :index }
 
-            it 'redirect to main contacts page' do
-                get :index
+            it 'collects an array of user contacts in @contacts' do
+                expect(assigns(:contacts)).to match_array(contacts)
+            end
 
+            it 'and @contacts does not collect another user contact' do
+                @current_user.contacts.each do |contact|
+                    expect(contact).to_not eq another_contact
+                end
+            end
+
+            it 'and renders main contacts page' do
                 expect(response).to render_template :index
             end
         end
